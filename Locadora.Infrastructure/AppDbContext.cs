216136737bb20ce8e3ffe873b3c05e8d9460874c
Victor Santos <dartinjs@gmail.com>
2022-04-08 13:35:48 -0300

    Mapping to Client

--- Locadora.Infrastructure/AppDbContext.cs
+++ Locadora.Infrastructure/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Locadora.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace Locadora.Infrastructure
 {
@@ -7,6 +8,11 @@ namespace Locadora.Infrastructure
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySQL("Server=localhost;Database=Locadora;Uid=root;Pwd=admin;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ClientMapping());
         }
     }
 }
---
+++ Locadora.Infrastructure/Mappings/ClientMapping.cs
@@ -0,0 +1,34 @@
+﻿using Locadora.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Locadora.Infrastructure.Mappings
+{
+    public class ClientMapping : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Name)
+                .HasColumnName("name")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(x => x.Document)
+                .HasColumnName("document")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(11)
+                .IsRequired();
+
+            builder.Property(x => x.BirthDay)
+                .HasColumnName("birth_day")
+                .HasColumnType("DATE")
+                .IsRequired();
+        }
+    }
+}
