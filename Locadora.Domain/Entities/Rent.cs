3df00ef6ed03e4ecf78e595b48f23725fcd6798d
Victor Santos <victor.santos@lambda3.com.br>
2022-04-08 12:07:37 -0300

    Adicionar arquivos de projeto.

---
+++ Locadora.Domain/Entities/Rent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Domain.Entities
+{
+    public class Rent
+    {
+        public int Id { get; set; }
+        public Client Client { get; set; }
+        public Movie Movie { get; set; }
+        public DateTime RentDate { get; private set; }
+        public DateTime ReturnDate { get; private set; }
+
+        public Rent()
+        {
+            RentDate = DateTime.UtcNow;
+            ReturnDate = Movie.IsReleased ? DateTime.UtcNow.AddDays(2) : DateTime.UtcNow.AddDays(3);
+        }
+    }
+}
