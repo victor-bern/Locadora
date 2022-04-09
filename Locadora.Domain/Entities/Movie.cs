39a95ba00b9fbf8a607fcd7684d59c7cd94ff16e
Victor Santos <dartinjs@gmail.com>
2022-04-09 14:04:10 -0300

    MovieController done

--- Locadora.Domain/Entities/Movie.cs
+++ Locadora.Domain/Entities/Movie.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Locadora.Domain.Entities
 {
     public class Movie
     {
         public int Id { get; set; }
+        [JsonPropertyName("Titulo")]
         public string Title { get; set; }
+        [JsonPropertyName("ClassificacaoIndicativa")]
         public int ParentalRating { get; set; }
+        [JsonPropertyName("Lancamento")]
         public bool IsReleased { get; set; }
     }
 }
---
+++ Locadora/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+﻿using Locadora.Domain.Entities;
+using Locadora.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.Controllers
+{
+    [Route("api/v1/movies")]
+    public class MovieController : BaseController<Movie, IMovieRepository>
+    {
+        public MovieController(IMovieRepository repository) : base(repository)
+        {
+        }
+    }
+}
