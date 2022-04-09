449a830ab6aa4df5779327d7a3b0acb0580711c9
Victor Santos <dartinjs@gmail.com>
2022-04-09 14:08:04 -0300

    RentController done

--- Locadora.Domain/Entities/Rent.cs
+++ Locadora.Domain/Entities/Rent.cs
@@ -1,13 +1,22 @@
-﻿namespace Locadora.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Locadora.Domain.Entities
 {
     public class Rent
     {
         public int Id { get; set; }
+        [JsonPropertyName("IdCliente")]
         public int ClientId { get; set; }
+        [JsonIgnore]
         public Client Client { get; set; }
+        [JsonPropertyName("IdFilme")]
         public int MovieId { get; set; }
+        [JsonIgnore]
         public Movie Movie { get; set; }
+
+        [JsonPropertyName("DataAluguel")]
         public DateTime RentDate { get; private set; }
+        [JsonPropertyName("DataDevolucao")]
         public DateTime ReturnDate { get; private set; }
 
         public Rent()
---
+++ Locadora/Controllers/RentController.cs
@@ -0,0 +1,14 @@
+﻿using Locadora.Domain.Entities;
+using Locadora.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.Controllers
+{
+    [Route("api/v1/rents")]
+    public class RentController : BaseController<Rent, IRentRepository>
+    {
+        public RentController(IRentRepository repository) : base(repository)
+        {
+        }
+    }
+}
