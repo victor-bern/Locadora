80d37078257f3755f97525abff166969c0977ee4
Victor Santos <dartinjs@gmail.com>
2022-04-10 16:39:10 -0300

    Modifying project folder

---
+++ BackEnd/Locadora.Infrastructure/Migrations/SeedMovieTable_202204101431.cs
@@ -0,0 +1,72 @@
+﻿using FluentMigrator;
+
+namespace Locadora.Infrastructure.Migrations
+{
+    [Migration(20220410143)]
+    public class SeedMovieTable_202204101431 : Migration
+    {
+        public override void Up()
+        {
+            Insert.IntoTable("Filme")
+             .Row(new
+             {
+                 Titulo = "O Poderoso Chefão",
+                 ClassificacaoIndicativa = 14,
+                 Lancamento = false
+             });
+            Insert.IntoTable("Filme")
+             .Row(new
+             {
+                 Titulo = "Harry Potter",
+                 ClassificacaoIndicativa = 0,
+                 Lancamento = false
+             });
+            Insert.IntoTable("Filme")
+             .Row(new
+             {
+                 Titulo = "Morbius",
+                 ClassificacaoIndicativa = 14,
+                 Lancamento = true
+             });
+            Insert.IntoTable("Filme")
+             .Row(new
+             {
+                 Titulo = "Green Book",
+                 ClassificacaoIndicativa = 10,
+                 Lancamento = false
+             });
+            Insert.IntoTable("Filme")
+             .Row(new
+             {
+                 Titulo = "Doutor Estranho No Multiverso da Loucura",
+                 ClassificacaoIndicativa = 14,
+                 Lancamento = true
+             });
+        }
+
+        public override void Down()
+        {
+            Delete.FromTable("Filme").Row(new
+            {
+                Titulo = "O Poderoso Chefão"
+            });
+            Delete.FromTable("Filme").Row(new
+            {
+                Titulo = "Harry Potter"
+            });
+            Delete.FromTable("Filme").Row(new
+            {
+                Titulo = "Morbius"
+            });
+            Delete.FromTable("Filme").Row(new
+            {
+                Titulo = "Green Book"
+            });
+            Delete.FromTable("Filme").Row(new
+            {
+                Titulo = "Doutor Estranho No Multiverso da Loucura"
+            });
+        }
+
+    }
+}
